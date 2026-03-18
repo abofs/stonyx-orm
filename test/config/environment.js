78acea1cde02b03b7a48b622e4caf3546b8f2e6f
Daniel DeLima <DanielDTech@gmail.com>
2026-03-17 21:13:51 -0300

    feat: add MySQL test database setup and configuration

--- test/config/environment.js
+++ test/config/environment.js
@@ -12,6 +12,16 @@ export default {
     db: {
       file: './test/sample/db.json',
       schema: './test/sample/db-schema.js'
+    },
+    mysql: {
+      host: 'localhost',
+      port: 3306,
+      user: 'stonyx_test',
+      password: 'stonyx_test',
+      database: 'stonyx_orm_test',
+      connectionLimit: 5,
+      migrationsDir: 'test/sample/migrations',
+      migrationsTable: '__test_migrations'
     }
   },
   restServer: {
